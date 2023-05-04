2cd41e1725ec8770bf8d7d252d160419026a1f91
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-05-04 11:56:05 +0000

    [Finish]応用課題2終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,15 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:update, :edit]
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = @book.user
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -32,15 +36,22 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.delete
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    user = Book.find(params[:id])
+    unless user.user_id == current_user.id
+      redirect_to books_path
+    end
   end
 end
