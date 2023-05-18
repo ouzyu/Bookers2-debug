d95c30f09deefc6998e158e58a4ff8ba304fb3a3
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-05-18 08:55:30 +0000

    [BackUp]

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,7 +4,7 @@ class RoomsController < ApplicationController
   def create
     @room = Room.create
     @current_entry = Entry.create(user_id: current_user.id, room_id: @room.id)
-    @another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)
+    @another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: @room.id)
     redirect_to room_path(@room)
   end
 
