bc0a61765ae24644b0357f0c94963a712c611f4a
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-05-21 04:59:35 +0000

    [Update]users/showページに7日間の投稿数を表示

--- app/models/book.rb
+++ app/models/book.rb
@@ -13,6 +13,12 @@ class Book < ApplicationRecord
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
 
+  scope :created_days_ago, ->(n) { where(created_at: n.days.ago.all_day) }
+
+  def self.past_week_count
+    (1..6).map { |n| created_days_ago(n).count }.reverse
+  end
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
