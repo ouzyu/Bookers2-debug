f733f465fc9285d1d6969042a306963104cd2d4b
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-05-23 08:24:00 +0000

    [Update]グループメールページ,フォームの作成

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'devise'
 gem 'jquery-rails'
 
 gem 'impressionist'
+
+gem 'dotenv-rails'
---
+++ test/mailers/previews/group_notice_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/group_notice_mailer
+class GroupNoticeMailerPreview < ActionMailer::Preview
+
+end
