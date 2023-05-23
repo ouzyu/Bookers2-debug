bccfe50e18630c8aad6a867bed03f8c19793dbd9
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-05-23 02:50:41 +0000

    [Finish]応用課題8c

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,6 +14,12 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def join
+    @group = Group.find(params[:group_id])
+    @group.users << current_user
+    redirect_to groups_path
+  end
+
   def new
     @group = Group.new
   end
@@ -21,10 +27,11 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
+    @group.users << current_user
     if @group.save
       redirect_to groups_path
     else
-      redirect_to request.referer
+      render "new"
     end
   end
 
@@ -38,6 +45,12 @@ class GroupsController < ApplicationController
     else
       render "edit"
     end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.users.delete(current_user)
+    redirect_to groups_path
   end
 
   private
