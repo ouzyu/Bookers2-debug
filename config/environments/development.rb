d407fc31c49f931a4f394b34c64994cc32347df0
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-05-24 03:29:22 +0000

    [Finish]応用課題9c

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: ENV["KEY"],
+    password: ENV["SECRET_KEY"],
+    anthentication: :login
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -74,18 +85,4 @@ Rails.application.configure do
   config.hosts.clear
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'gmail.com',
-    user_name: ENV['KEY'],
-    password: ENV['SECRET_KEY'],
-    authentication: 'login',
-    enable_starttls_auto: true,
-  }
-
-  config.action_mailer.asset_host = 'http://localhost:3000'
-  config.action_mailer.default_url_options = { host: 'localhost',port:3000 }
 end
